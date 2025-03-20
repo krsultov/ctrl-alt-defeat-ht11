d9257a173e93b9b4028e0503cab9e060d15be066
NikolayGrozev <nikigr2009@abv.bg>
2025-03-20 22:16:15 +0200

    Removed ShadCN UI

--- frontend/src/app/Register/page.tsx
+++ frontend/src/app/Register/page.tsx
@@ -1,4 +1,4 @@
-import { RegisterForm } from "@/components/register-form";
+
 import Link from "next/link";
 
 export default function Register(){
@@ -6,13 +6,6 @@ export default function Register(){
         <div>
             <div className="text-3xl">Register</div>
             <Link href={"/"}>Home</Link>
-            <div>
-                <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
-                    <div className="w-full max-w-sm">
-                        <RegisterForm />
-                    </div>
-                </div>
-            </div>
         </div>
     );
 }
