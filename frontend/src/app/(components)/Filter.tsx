eb2e752f92291a619099bfb0b1fabece3465ffd2
VictorBotev12 <160576550+VictorBotev12@users.noreply.github.com>
2025-03-22 03:03:04 +0200

    Filter fix

--- frontend/src/app/(components)/Filter.tsx
+++ frontend/src/app/(components)/Filter.tsx
@@ -35,26 +35,24 @@ export default function Filter() {
                 }
             }}>
                 <div className="w-full flex justify-center">
-                    <div className="bg-[#E6E6E6] flex flex-col border rounded-xl border-transparent max-w-[90%] px-4">
+                    <div className="bg-[#E6E6E6] flex flex-col border rounded-xl border-transparent w-[90%] px-4">
                         <div className="flex mb-4 justify-start ">
                             <Typography className="p-3" variant="h6" fontWeight={300}>
                                 Filters
                             </Typography>
                         </div>
-                        <div className="mb-5 pb-3 flex flex-col items-center w-full">
-                            <div className="flex flex-row items-center">
-                                <div className="w-[40%] flex-1/3 sm:">
+                        <div className="mb-5 pb-3 flex flex-col justify-center items-center w-full">
+                            <div className="flex flex-row items-center px-3 w-full gap-4">
+                                <div className="w-[40%] flex-1/3 ">
                                     <SelectAutoWidth />
                                 </div>
-                                <div className="flex flex-2/3 items-center">
-                                    <DatePicker />
-                                </div>
-                            </div>
-                            <div className="flex gap-2 items-center">
-                                <div className="flex items-center flex-col sm:flex-row gap-5">
-                                    <RangeManager />
+                                <div className="flex flex-1/3 items-center">
                                     <RangeSlider />
                                 </div>
+                                <div className="flex flex-1/3 items-center">
+                                    <DatePicker />
+                                </div>
+                                
                             </div>
                         </div>
                     </div>
